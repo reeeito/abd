ef354e8f9fe2a9623e91d6f26ff27fc602534cc9
reeeito <barca.0716@i.softbank.jp>
2021-04-15 06:59:23 +0900

    テーブル作成

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  def new
+  end
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
